e3218ee36cb3cb306ba26ede1922fe7e8982e9e4
StevenDRiggs <sfriggs21@gmail.com>
2020-08-12 09:55:40 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const submit = document.querySelector('form#create-task-form input[type=submit]')
+
+  submit.addEventListener('click', addTaskToList)
+
+  function addTaskToList(event) {
+    event.preventDefault()
+
+    const newTask = document.querySelector('input#new-task-description')
+    const taskList = document.querySelector('ul#tasks')
+
+    const task = document.createElement('li')
+
+    taskList.appendChild(task)
+    task.textContent = newTask.value
+
+    newTask.value = ''
+  }
 });
