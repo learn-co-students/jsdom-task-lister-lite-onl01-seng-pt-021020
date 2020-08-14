9f2b5bca5caca80f7f00bfbcc39aaa587e99f428
08fdange <08fdange@gmail.com>
2020-08-13 20:07:54 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.getElementById('create-task-form');
+  // New Task Submit Event
+  form.addEventListener('submit', (event) => {
+    let newTaskForm = form.elements['new-task-description'];
+    let newTask = newTaskForm.value;
+    let ul = document.getElementById('tasks');
+    let li = document.createElement('li');
+    li.innerHTML = newTask + "<button onclick=''>X</button>";
+    ul.appendChild(li);
+    event.preventDefault();
+  })
+
 });
