84b0d743ab12301896f51b9ba8625b3678f60b68
tyler jones <rteyl1690@gmail.com>
2020-08-15 21:28:46 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener('DOMContentLoaded', () => {
+	document.getElementById('submit').addEventListener('click', function(e) {
+		e.preventDefault();
+		const newText = document.getElementById('new-task-description').value;
+		const task = document.getElementById('tasks');
+		task.appendChild(document.createElement('li')).innerHTML = newText;
+	});
+  document.getElementById('create-task-form').reset();
 });
