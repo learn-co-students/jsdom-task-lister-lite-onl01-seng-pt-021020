15a869a6c98d2d438e87ae212efae342f9962bc0
Awilliams4321 <awilliams1234@live.com>
2020-08-18 11:46:10 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById('create-task-form')
+  form.addEventListener('submit', submitPrevent)
 });
+
+function submitPrevent(e) {
+  let input = document.getElementById('new-task-description').value
+  e.preventDefault();
+
+  const li = ocument.createElement('li');
+  li.innerText = input;
+  document.getElementById('tasks').appendChild(li);
+   
+}
