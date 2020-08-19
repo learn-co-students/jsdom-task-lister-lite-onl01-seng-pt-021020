5716c5a31de2ef04f1cffcbb354bda787301001d
Hurly77 <cameron.leverett@yahoo.com>
2020-08-18 19:21:47 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  // your code here  
+  let form = document.getElementById("create-task-form");
+  let ul = document.getElementById("tasks");
+  let task = document.getElementById("new-task-description");
+
+  
+  function addTask(e){
+    let li = document.createElement('li')
+    ul.appendChild(li).innerHTML = `${task.value} `
+    let button = document.createElement('button')
+    li.appendChild(button).innerHTML = 'x';
+    e.target.reset();
+    e.preventDefault()
+  }
+
+  ul.addEventListener("click", function(e){
+    let tgt = e.target;
+    if (tgt.tagName === "BUTTON") {
+      tgt.parentNode.remove();
+    }
+  });
+
+  form.addEventListener("submit", addTask);
 });
